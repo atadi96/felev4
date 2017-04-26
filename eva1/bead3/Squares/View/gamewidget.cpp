cfe01aa1c105cca9b73a43ec1473089230801dc1
Abonyi-Tóth Ádám <atadi96@gmail.com>
2017-04-27 00:43:25 +0200

    eva1-bead3 - laying down the basics of the game widget

--- eva1/bead3/Squares/View/gamewidget.cpp
+++ eva1/bead3/Squares/View/gamewidget.cpp
@@ -4,3 +4,41 @@ GameWidget::GameWidget(QWidget *parent) : QWidget(parent)
 {
 
 }
+
+void GameWidget::paintEvent(QPaintEvent* ) {
+    m_painter.begin(this);
+    //paint background
+    //paint squares
+    //paint lines
+    //paint line under mouse
+    m_painter.end();
+}
+
+QLine GameWidget::toWorld(const QLine& line) const {
+    line *= m_unit;
+    line.translate(m_brushWidth / 2, m_brushWidth / 2);
+}
+QRect GameWidget::toWorld(const QPoint& coord) const {
+    return QRect(
+        worldPoint(coord.x(), coord.y()),
+        QSize(m_unit, m_unit)
+    );
+}
+
+QPoint GameWidget::worldPoint(int x, int y) const {
+    return QPoint(
+        x * m_unit + m_brushWidth / 2,
+        y * m_unit + m_brushWidth / 2,
+    );
+}
+
+QColor GameWidget::toColor(Player player) const {
+    switch(player) {
+    case Player::Blue:
+        return Qt::blue;
+    case Player::Red:
+        return Qt::red;
+    case Player::None:
+        return Qt::black;
+    }
+}
--- eva1/bead3/Squares/View/gamewidget.h
+++ eva1/bead3/Squares/View/gamewidget.h
@@ -2,13 +2,36 @@
 #define GAMEWIDGET_H
 
 #include <QWidget>
+#include <QPainter>
+#include <QRect>
+#include <QLine>
+#include <QColor>
+
+#include "Model/game.h"
 
 class GameWidget : public QWidget
 {
     Q_OBJECT
+private:
+    QPainter m_painter;
+
+    int m_mapSize = 9;
+    int m_brushWidth = 3;
+    int m_unit = 16;
+
 public:
     explicit GameWidget(QWidget *parent = 0);
-signals:
+
+protected:
+    void paintEvent(QPaintEvent* );
+
+private:
+    QLine toWorld(const QLine&) const;
+    QRect toWorld(const QPoint&) const;
+    QPoint worldPoint(int x, int y) const;
+    QColor toColor(Player) const;
+
+public slots:
 
 };
 
